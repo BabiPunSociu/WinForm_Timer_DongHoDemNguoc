3fb6b67fa15055c90eb09ffffd5468ff925050d6
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-06 23:40:36 +0700

    DungNguyen

---
+++ Bai15_Trang78_DongHoDemNguoc/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Bai15_Trang78_DongHoDemNguoc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPhut = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtGiay = new System.Windows.Forms.TextBox();
+            this.btnChay = new System.Windows.Forms.Button();
+            this.btnDung = new System.Windows.Forms.Button();
+            this.btnKetThuc = new System.Windows.Forms.Button();
+            this.tmrDongHo = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(51, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "(Phút)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(300, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "(Giây)";
+            // 
+            // txtPhut
+            // 
+            this.txtPhut.BackColor = System.Drawing.Color.LightGreen;
+            this.txtPhut.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhut.Location = new System.Drawing.Point(66, 67);
+            this.txtPhut.Name = "txtPhut";
+            this.txtPhut.Size = new System.Drawing.Size(165, 45);
+            this.txtPhut.TabIndex = 0;
+            this.txtPhut.Enter += new System.EventHandler(this.txtPhut_Enter);
+            this.txtPhut.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPhut_KeyPress);
+            this.txtPhut.MouseEnter += new System.EventHandler(this.txtPhut_MouseEnter);
+            this.txtPhut.MouseLeave += new System.EventHandler(this.txtPhut_MouseLeave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(257, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 38);
+            this.label3.TabIndex = 4;
+            this.label3.Text = ":";
+            // 
+            // txtGiay
+            // 
+            this.txtGiay.BackColor = System.Drawing.Color.LightGreen;
+            this.txtGiay.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGiay.Location = new System.Drawing.Point(307, 67);
+            this.txtGiay.Name = "txtGiay";
+            this.txtGiay.Size = new System.Drawing.Size(165, 45);
+            this.txtGiay.TabIndex = 1;
+            this.txtGiay.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPhut_KeyPress);
+            this.txtGiay.MouseEnter += new System.EventHandler(this.txtPhut_MouseEnter);
+            this.txtGiay.MouseLeave += new System.EventHandler(this.txtPhut_MouseLeave);
+            // 
+            // btnChay
+            // 
+            this.btnChay.Location = new System.Drawing.Point(24, 169);
+            this.btnChay.Name = "btnChay";
+            this.btnChay.Size = new System.Drawing.Size(157, 46);
+            this.btnChay.TabIndex = 2;
+            this.btnChay.Text = "&Chạy";
+            this.btnChay.UseVisualStyleBackColor = true;
+            this.btnChay.Click += new System.EventHandler(this.btnChay_Click);
+            this.btnChay.MouseEnter += new System.EventHandler(this.btnChay_MouseEnter);
+            this.btnChay.MouseLeave += new System.EventHandler(this.btnChay_MouseLeave);
+            // 
+            // btnDung
+            // 
+            this.btnDung.Location = new System.Drawing.Point(216, 169);
+            this.btnDung.Name = "btnDung";
+            this.btnDung.Size = new System.Drawing.Size(157, 46);
+            this.btnDung.TabIndex = 5;
+            this.btnDung.Text = "&Dừng";
+            this.btnDung.UseVisualStyleBackColor = true;
+            this.btnDung.Click += new System.EventHandler(this.btnDung_Click);
+            // 
+            // btnKetThuc
+            // 
+            this.btnKetThuc.Location = new System.Drawing.Point(405, 169);
+            this.btnKetThuc.Name = "btnKetThuc";
+            this.btnKetThuc.Size = new System.Drawing.Size(157, 46);
+            this.btnKetThuc.TabIndex = 6;
+            this.btnKetThuc.Text = "&Kết thúc";
+            this.btnKetThuc.UseVisualStyleBackColor = true;
+            this.btnKetThuc.Click += new System.EventHandler(this.btnKetThuc_Click);
+            // 
+            // tmrDongHo
+            // 
+            this.tmrDongHo.Tick += new System.EventHandler(this.tmrDongHo_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(582, 237);
+            this.Controls.Add(this.btnKetThuc);
+            this.Controls.Add(this.btnDung);
+            this.Controls.Add(this.btnChay);
+            this.Controls.Add(this.txtGiay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPhut);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đồng hồ đếm ngược _ Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPhut;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtGiay;
+        private System.Windows.Forms.Button btnChay;
+        private System.Windows.Forms.Button btnDung;
+        private System.Windows.Forms.Button btnKetThuc;
+        private System.Windows.Forms.Timer tmrDongHo;
+    }
+}
+
---
+++ Bai15_Trang78_DongHoDemNguoc/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai15_Trang78_DongHoDemNguoc
+{
+    public partial class Form1 : Form
+    {
+        ulong tongTG;   // Đơn vị: giây (s)
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tmrDongHo.Interval = 1000;  // 1s
+            txtPhut.MaxLength = 2;
+            txtGiay.MaxLength = 2;
+        }
+
+        private void txtPhut_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // chỉ được nhập số
+            if(!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void btnChay_MouseEnter(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.BackColor = Color.LightBlue;
+            btn.ForeColor = Color.Red;
+        }
+
+        private void btnChay_MouseLeave(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.BackColor = Color.White;
+            btn.ForeColor = Color.Black;
+        }
+
+        private void btnChay_Click(object sender, EventArgs e)
+        {
+            if(txtPhut.Text.Trim() == "")
+            {
+                MessageBox.Show("Bạn phải nhập số phút");
+                txtPhut.Focus();
+                return;
+            }
+            if (txtGiay.Text.Trim() == "" || int.Parse(txtGiay.Text.Trim())>60)
+            {
+                MessageBox.Show("Bạn phải nhập số giây trong đoạn [0->60]");
+                txtGiay.Clear();
+                txtGiay.Focus();
+                return;
+            }
+
+            tongTG = ulong.Parse(txtPhut.Text.Trim())*60 + ulong.Parse(txtGiay.Text.Trim());
+            tmrDongHo.Start();
+        }
+
+        private void tmrDongHo_Tick(object sender, EventArgs e)
+        {
+            if (tongTG > 0)
+            {
+                tongTG -= 1;
+                txtPhut.Text = (tongTG / 60).ToString();
+                txtGiay.Text = (tongTG % 60).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Báo thức ^^");
+                tmrDongHo.Stop();
+            }
+        }
+
+        private void btnDung_Click(object sender, EventArgs e)
+        {
+            tmrDongHo.Stop();
+        }
+
+        private void btnKetThuc_Click(object sender, EventArgs e)
+        {
+            tongTG = 0;
+            txtPhut.Text = (tongTG / 60).ToString();
+            txtGiay.Text = (tongTG % 60).ToString();
+
+        }
+
+        private void txtPhut_MouseEnter(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox) sender;
+            txt.BackColor = Color.Yellow;
+            txt.ForeColor = Color.Green;
+        }
+
+        private void txtPhut_MouseLeave(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            txt.BackColor = Color.White;
+            txt.ForeColor = Color.Black;
+        }
+
+        private void txtPhut_Enter(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            txt.BackColor = Color.Yellow;
+            txt.ForeColor = Color.Green;
+        }
+    }
+}
